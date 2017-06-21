414396a0c870831bd392122eef5884e9207b58cc
vymiyai <vymiyai@gmail.com>
2017-06-21 18:05:45 +0200

    Added host as an environment variable, added logic to the rest controller.

--- src/main/java/com/rhcloud/tutorials/spring_postgres/HomeController.java
+++ src/main/java/com/rhcloud/tutorials/spring_postgres/HomeController.java
@@ -1,14 +1,26 @@
 package com.rhcloud.tutorials.spring_postgres;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+	@Autowired
+	private DiscordUserRepository repository;
+
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		StringBuilder sb = new StringBuilder();
+		sb.append("Database entries:\n\n");
+		
+		for (DiscordUser user : this.repository.findAll()) {
+			sb.append(user.toString());
+			sb.append('\n');
+		}
+
+		return sb.toString();
 	}
 
 }
