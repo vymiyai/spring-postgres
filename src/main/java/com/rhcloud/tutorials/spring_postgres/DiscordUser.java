7f8446926da05dc5791de5bdf9988b67cffe3001
vymiyai <vymiyai@gmail.com>
2017-06-21 16:05:45 +0200

    Adjusted configurations locally.

--- src/main/java/com/rhcloud/tutorials/spring_postgres/DiscordUser.java
+++ src/main/java/com/rhcloud/tutorials/spring_postgres/DiscordUser.java
@@ -1,6 +1,6 @@
 package com.rhcloud.tutorials.spring_postgres;
 
-import java.sql.Date;
+import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,17 +14,17 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Entity
 public class DiscordUser {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(unique = true)
 	private String username;
-
+	
 	@CreationTimestamp
-	private Date creation_date;
+	private Timestamp creation_date;
 
 	@UpdateTimestamp
-	private Date last_change;
+	private Timestamp last_change;
 
 	private String access_token;
 
@@ -38,7 +38,7 @@ public class DiscordUser {
 
 	@Override
 	public String toString() {
-		return String.format("Customer[id='%d', username='%s'creation_date='%s', last_change='%s', access_token='%s']",
+		return String.format("Discord user [id='%d', username='%s'creation_date='%s', last_change='%s', access_token='%s']",
 				this.id, this.username, this.creation_date, this.last_change, this.access_token);
 	}
 
